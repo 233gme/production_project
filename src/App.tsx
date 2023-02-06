85fed43a547aef01930769cb3a2ffedae2f4e54b
Vadim Brk <Vadim.Brk@outlook.com>
2023-02-06 03:22:37 -0300

    1.6 Added Routes, Lazy Suspence, fix tsconfig;

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import Counter from "./components/Counter";
+import React, {Suspense} from 'react';
+import {Link, Route, Routes} from 'react-router-dom';
+import {AboutPageAsync} from "./pages/AboutPages/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPages/MainPage.async";
 
 const App = () => {
     return (
         <div>
-            <Counter />
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync />}/>
+                    <Route path={'/'} element={<MainPageAsync/>}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
