16fb05932d5d4c044446d38846b635d599287189
Vadim Brk <Vadim.Brk@outlook.com>
2023-02-07 23:26:16 -0300

    1.10 Added some architecture, fixed imports;

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import React, {Suspense} from "react";
 import {Link, Route, Routes} from "react-router-dom";
-import {AboutPageAsync} from "./pages/AboutPages/AboutPage.async";
-import {MainPageAsync} from "./pages/MainPages/MainPage.async";
-import {useTheme} from "./theme/useTheme";
+import {AboutPage} from "pages/AboutPages/";
+import {MainPage} from "pages/MainPages";
+import {useTheme} from "app/providers/ThemeProvider";
+import {classNames} from "shared/lib/classNames/classNames";
 import "./styles/index.scss";
-import {classNames} from "./helpers/classNames/classNames";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
@@ -18,8 +18,8 @@ const App = () => {
 
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync />}/>
-                    <Route path={'/'} element={<MainPageAsync/>}/>
+                    <Route path={'/about'} element={<AboutPage/>}/>
+                    <Route path={'/'} element={<MainPage/>}/>
                 </Routes>
             </Suspense>
         </div>
--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -1,21 +1,21 @@
 import {useContext} from "react";
-import {LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext} from "./ThemeContext";
+import {LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext} from "app/providers/ThemeProvider/lib/ThemeContext";
 
 interface UseThemeResult {
     toggleTheme: () => void,
     theme: Theme,
 }
 
-export function useTheme(): UseThemeResult {
-    const {theme, setTheme} = useContext(ThemeContext)
+export function useTheme (): UseThemeResult {
+    const {theme, setTheme} = useContext(ThemeContext);
     const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
     const toggleTheme = () => {
         localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
         setTheme(newTheme);
-    }
+    };
 
     return {
         theme,
         toggleTheme,
-    }
+    };
 }
