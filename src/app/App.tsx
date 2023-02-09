e66f3ea10ca58f991d01ea081b877328a8425b4b
Vadim Brk <Vadim.Brk@outlook.com>
2023-02-09 00:23:36 -0300

    2.12 Navbar. First UI component;

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,18 @@
 import React from "react";
-import {Link} from "react-router-dom";
 import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
 import {AppRouter} from "app/providers/router";
 import "./styles/index.scss";
+import {Navbar} from "widgets/Navbar";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
 
     return (
         <div className={classNames("app", {}, [theme])}>
-            <button onClick={toggleTheme}>Toggle Theme</button>
+            <Navbar />
 
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
+            <button onClick={toggleTheme}>Toggle Theme</button>
 
             <AppRouter />
         </div>
