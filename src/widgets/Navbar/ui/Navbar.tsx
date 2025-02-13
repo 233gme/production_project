deb8ed1354457b80827488707dc56f4fc6052003
Vadim Brk <Vadim.Brk@outlook.com>
2025-02-13 09:12:28 +0300

    19 Stylelint. Plugin for i18next

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,18 +1,20 @@
 import { FC } from 'react';
 import cls from './Navbar.module.scss';
 import { AppLink, AppLinkTheme, classNames } from 'shared';
+import { useTranslation } from 'react-i18next';
 
 interface Props {
 	className?: string;
 }
 
 export const Navbar: FC<Props> = ({ className }) => {
+	const { t } = useTranslation();
 
 	return (
 		<header className={classNames(cls.wrapper, {}, [className])}>
 			<nav className={cls.nav}>
-				<AppLink theme={AppLinkTheme.SECONDARY} to="/">Main</AppLink>
-				<AppLink theme={AppLinkTheme.SECONDARY} to="/about">About</AppLink>
+				<AppLink theme={AppLinkTheme.SECONDARY} to="/">{t('Home')}</AppLink>
+				<AppLink theme={AppLinkTheme.SECONDARY} to="/about">{t('About')}</AppLink>
 			</nav>
 		</header>
 	);
--- src/widgets/Sitebar/ui/Sitebar.tsx
+++ src/widgets/Sitebar/ui/Sitebar.tsx
@@ -2,12 +2,14 @@ import { FC, useState } from 'react';
 import cls from './Sitebar.module.scss';
 import { classNames } from 'shared';
 import { LanguageSwitcher, ThemeSwitcher } from 'widgets';
+import { useTranslation } from 'react-i18next';
 
 type Props = {
 	className?: string
 }
 
 export const Sitebar: FC<Props> = ({ className }) => {
+	const { t } = useTranslation();
 	const [collapsed, setCollapsed] = useState(false);
 
 	const onToggle = () => {
@@ -16,7 +18,7 @@ export const Sitebar: FC<Props> = ({ className }) => {
 
 	return (
 		<div className={classNames(cls.wrapper, { [cls.collapsed]: collapsed }, [className])}>
-			<button onClick={onToggle}>toggle</button>
+			<button onClick={onToggle}>{t('toggle')}</button>
 
 			<div className={cls.footer}>
 				<ThemeSwitcher/>
